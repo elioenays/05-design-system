8e360a4a7c8f2f26752b020d91eb7a1993cf555f
Elioenay <elionay407@gmail.com>
2023-08-15 17:39:39 -0300

    Config storybook

---
+++ packages/react/src/index.tsx
@@ -0,0 +1,11 @@
+import { styled } from './styles'
+
+const Button = styled('button', {
+  fontFamily: '$default',
+  background: '$ignite300',
+  borderRadius: '$md',
+})
+
+export function App() {
+  return <Button>Hello World</Button>
+}
