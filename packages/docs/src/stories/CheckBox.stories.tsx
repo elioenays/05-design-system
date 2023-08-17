14b44af18b0117cde7050d450ed6702dd217ce8e
Elioenay <elionay407@gmail.com>
2023-08-17 16:18:23 -0300

    Componente: Checkbox

---
+++ packages/docs/src/stories/CheckBox.stories.tsx
@@ -0,0 +1,22 @@
+import { Box, Text, CheckBox, CheckBoxProps } from '@ignite-ui/react'
+import type { Meta, StoryObj } from '@storybook/react'
+
+export default {
+  title: 'Form/CheckBox',
+  component: CheckBox,
+  decorators: [
+    (Story) => {
+      return (
+        <Box
+          as='label'
+          css={{ display: 'flex', flexDirection: 'row', gap: '$2' }}
+        >
+          {Story()}
+          <Text size='sm'>Accept terms of use</Text>
+        </Box>
+      )
+    },
+  ],
+} as Meta<CheckBoxProps>
+
+export const Primary: StoryObj<CheckBoxProps> = {}
--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -1,3 +1,21 @@
 module.exports = {
   extends: ['@rocketseat/eslint-config/react'],
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        arrowParens: 'always',
+        singleQuote: true,
+        trailingComma: 'all',
+        jsxSingleQuote: true,
+        bracketSpacing: true,
+        bracketSameLine: false,
+        singleAttributePerLine: true,
+        semi: false,
+        endOfLine: 'lf',
+        printWidth: 80,
+        tabWidth: 2,
+      },
+    ],
+  },
 }
