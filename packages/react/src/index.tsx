d555c87c3bad07ecdb09d67fb4296d79a23b2c8f
Elioenay <elionay407@gmail.com>
2023-08-17 15:20:58 -0300

    Componente: TextInput

--- packages/react/src/index.tsx
+++ packages/react/src/index.tsx
@@ -1,5 +1,6 @@
 export * from './components/Box'
 export * from './components/Text'
 export * from './components/Heading'
-export * from './components/avatar'
+export * from './components/Avatar'
 export * from './components/Button'
+export * from './components/TextInput'
