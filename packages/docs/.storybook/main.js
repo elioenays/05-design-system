aa99affeeadcdab9d508c3fad4e8fb71ddce9b9b
Elioenay <elionay407@gmail.com>
2023-08-17 17:28:39 -0300

    Addon de acessibilidade

--- packages/docs/.storybook/main.js
+++ packages/docs/.storybook/main.js
@@ -17,6 +17,7 @@ const config = {
     getAbsolutePath('@storybook/addon-onboarding'),
     getAbsolutePath('@storybook/addon-interactions'),
     getAbsolutePath('storybook-dark-mode'),
+    getAbsolutePath('@storybook/addon-a11y'),
   ],
   framework: {
     name: getAbsolutePath('@storybook/react-vite'),
