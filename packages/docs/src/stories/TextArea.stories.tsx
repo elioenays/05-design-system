55c9e440fcb7e9a2d768a46de4567f6607161b4d
Elioenay <elionay407@gmail.com>
2023-08-17 15:28:50 -0300

    Componente: TextArea

---
+++ packages/docs/src/stories/TextArea.stories.tsx
@@ -0,0 +1,28 @@
+import { Box, Text, TextArea, TextAreaProps } from '@ignite-ui/react'
+import type { Meta, StoryObj } from '@storybook/react'
+
+export default {
+  title: 'Form/Text Area',
+  component: TextArea,
+  decorators: [
+    (story) => {
+      return (
+        <Box
+          as='label'
+          css={{ display: 'flex', flexDirection: 'column', gap: '$2' }}
+        >
+          <Text size='sm'>Observations</Text>
+          {story()}
+        </Box>
+      )
+    },
+  ],
+} as Meta<TextAreaProps>
+
+export const Primary: StoryObj<TextAreaProps> = {
+  args: { placeholder: 'Add any observations' },
+}
+
+export const Disabled: StoryObj<TextAreaProps> = {
+  args: { disabled: true },
+}
